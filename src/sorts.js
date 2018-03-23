cc60309bce972e8e571f50023799b06ef1ce5344
Kerin <kerinhayes@gmail.com>
2018-03-23 09:14:06 -0400

    merge

--- src/sorts.js
+++ src/sorts.js
@@ -26,7 +26,7 @@ function* selectionSort(arr) {
   }
 }
 
-// function* merge (arr1, arr2) {
+// function merge (arr1, arr2) {
 //   let merged = [];
 //
 //   while (arr1.length && arr2.length) {
@@ -44,21 +44,99 @@ function* selectionSort(arr) {
 //
 // function* mergeSort(arr) {
 //
-//     if (arr.length <= 1) { return arr; }
-//
-//     let mid = Math.floor(arr.length/2);
-//
-//     let left = mergeSort(arr.slice(0, mid));
-//     let right = mergeSort(arr.slice(mid));
+//     if (arr.length <= 1) { yield arr; }
 //
 //
-//     yield merge(left, right);
+//       let mid = Math.floor(arr.length/2);
+//       yield arr;
 //
+//       let left = mergeSort(arr.slice(0, mid));
+//       let right = mergeSort(arr.slice(mid));
+//
+//
+//       yield merge(left, right);
 //
 // }
 
+function* mergeSortBottomUp(array) {
+  var step = 1;
+  while (step < array.length) {
+    var left = 0;
+    while (left + step < array.length) {
+      mergeBottomUp(array, left, step);
+      left += step * 2;
+      yield array;
 
-function quickSort(arr) {
+    }
+    step *= 2;
+
+  }
+}
+
+function mergeBottomUp(array, left, step) {
+  var right = left + step;
+  var end = Math.min(left + step * 2 - 1, array.length - 1);
+  var leftMoving = left;
+  var rightMoving = right;
+  var temp = [];
+
+  for (var i = left; i <= end; i++) {
+    if ((array[leftMoving] <= array[rightMoving] || rightMoving > end) &&
+        leftMoving < right) {
+      temp[i] = array[leftMoving];
+      leftMoving++;
+    } else {
+      temp[i] = array[rightMoving];
+      rightMoving++;
+    }
+  }
+
+  for (var j = left; j <= end; j++) {
+    array[j] = temp[j];
+  }
+}
+
+function* qsort(arr) {
+    var stack = [arr];
+    var sorted = [];
+
+    while (stack.length) {
+        var temp = stack.pop(), tl = temp.length;
+
+        if (tl === 1) {
+            sorted.push(temp[0]);
+            continue;
+        }
+        var pivot = temp[0];
+        var left = [], right = [];
+
+        for (var i = 1; i < tl; i++) {
+            if (temp[i] < pivot) {
+                left.push(temp[i]);
+            } else {
+                right.push(temp[i]);
+            }
+            yield temp;
+        }
+
+        left.push(pivot);
+
+        if (right.length)
+            stack.push(right);
+        if (left.length)
+            stack.push(left);
+
+
+    }
+
+    yield sorted;
+
+
+}
+
+
+function* quickSort(arr) {
+  debugger;
 
     if (arr.length <= 1) { return arr; }
 
@@ -74,4 +152,4 @@ function quickSort(arr) {
       return left.concat([pivot]).concat(right);
     }
 
-    module.exports = { bubbleSort, quickSort, selectionSort };
+  module.exports = { bubbleSort, qsort, selectionSort, mergeSortBottomUp };
--- src/visuals.js
+++ src/visuals.js
@@ -6,38 +6,39 @@ class Visuals {
     this.nums = [];
     this.ctx = ctx;
     this.colorMap = {
-      1: 'rgb(255, 51, 51)',
-      2: 'rgb(255, 102, 51)',
-      3: 'rgb(255, 153, 51)',
-      4: 'rgb(255, 204, 51)',
-      5: 'rgb(255, 255, 51)',
-      6: 'rgb(204, 255, 51)',
-      7: 'rgb(153, 255, 51)',
-      8: 'rgb(102, 255, 51)',
-      9: 'rgb(51, 255, 51)',
-      10: 'rgb(51, 255, 102)',
-      11: 'rgb(51, 255, 153)',
-      12: 'rgb(51, 255, 204)',
-      13: 'rgb(51, 255, 255)',
-      14: 'rgb(51, 204, 255)',
-      15: 'rgb(51, 153, 255)',
-      16: 'rgb(51, 102, 255)',
-      17: 'rgb(51, 51, 255)',
-      18: 'rgb(102, 51, 255)',
-      19: 'rgb(153, 51, 255)',
-      20: 'rgb(204, 51, 255)',
-      21: 'rgb(255, 51, 255)',
-      22: 'rgb(255, 51, 204)',
-      23: 'rgb(255, 51, 153)',
-      24: 'rgb(255, 51, 119)',
-      25: 'rgb(255, 51, 102)',
-      26: 'rgb(255, 51, 51)'
+      1: 'rgba(255, 51, 51, .75)',
+      2: 'rgba(255, 102, 51, .75)',
+      3: 'rgba(255, 153, 51, .75)',
+      4: 'rgba(255, 204, 51, .75)',
+      5: 'rgba(255, 255, 51, .75)',
+      6: 'rgba(204, 255, 51, .75)',
+      7: 'rgba(153, 255, 51, .75)',
+      8: 'rgba(102, 255, 51, .75)',
+      9: 'rgba(51, 255, 51, .75)',
+      10: 'rgba(51, 255, 102, .75)',
+      11: 'rgba(51, 255, 153, .75)',
+      12: 'rgba(51, 255, 204, .75)',
+      13: 'rgba(51, 255, 255, .75)',
+      14: 'rgba(51, 204, 255, .75)',
+      15: 'rgba(51, 153, 255, .75)',
+      16: 'rgba(51, 102, 255, .75)',
+      17: 'rgba(51, 51, 255, .75)',
+      18: 'rgba(102, 51, 255, .75)',
+      19: 'rgba(153, 51, 255, .75)',
+      20: 'rgba(204, 51, 255, .75)',
+      21: 'rgba(255, 51, 255, .75)',
+      22: 'rgba(255, 51, 204, .75)',
+      23: 'rgba(255, 51, 153, .75)',
+      24: 'rgba(255, 51, 119, .75)',
+      25: 'rgba(255, 51, 102, .75)',
+      26: 'rgba(255, 51, 51, .75)'
     };
 
       this.popArray();
       this.shuffle = this.shuffleArray(this.nums);
       this.draw();
       this.goDraw();
+
 
 
 
@@ -58,6 +59,7 @@ class Visuals {
 
     for(let j = 0; j < 26; j++) {
       for(let i = 0; i < 20; i++) {
+
         this.nums.push(keys[j]);
       }
     }
@@ -66,15 +68,18 @@ class Visuals {
 
   draw() {
     this.nums.forEach((num, idx)=> {
+      this.ctx.clearRect(idx, 0, 1, 520);
       this.ctx.fillStyle = this.colorMap[num];
-      this.ctx.fillRect(idx, 0, 1, 500);
+      this.ctx.fillRect(idx, 0, 1, 520);
+
+
     });
 
   }
 
   goDraw() {
 
-    const sort = Sorts.selectionSort(this.shuffle);
+    const sort = Sorts.mergeSortBottomUp(this.shuffle);
 
     const animate = () => {
       requestAnimationFrame(animate);
